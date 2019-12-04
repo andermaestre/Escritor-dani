5ccbf28af22336c5c2acddb0227915c5c58f3945
Ander Maestre <andermaestre_90@hotmail.es>
2019-12-04 13:44:49 +0100

    Ejemplo

---
+++ Escritor/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+namespace Escritor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   Mutex m;
+            String pal;
+            bool nuevo;
+            StreamWriter sw;
+            ////Comprobar si hay lectores
+            do
+            {
+                m = new Mutex(false, "MutexLector", out nuevo);
+                m.Dispose();
+            } while (!nuevo);
+
+            //Crear Mutex escritro
+            m = new Mutex(false, "MutexEscritor");
+            m.WaitOne();
+
+            sw = new StreamWriter("..\\..\\..\\Fichero.txt", true); //true significa modo concatenación.
+            Console.WriteLine("Palabra:");
+           
+            pal = Console.ReadLine();
+            while(pal!="")
+            {   
+                sw.WriteLine(pal);
+                Console.WriteLine("Palabra:");
+                pal = Console.ReadLine();
+            }
+            sw.Close();
+
+           m.ReleaseMutex();
+            m.Dispose();
+        }
+    }
+}
---
+++ Lector/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Lector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            StreamReader sr;
+
+            Mutex m;
+            bool nuevo;
+          do
+            {
+                m = new Mutex(false, "MutexEscritor", out nuevo);
+                m.Dispose();
+            } while (!nuevo);
+            m = new Mutex(false, "MutexLector");
+            sr = new StreamReader("..\\..\\..\\Fichero.txt");
+            String pal = sr.ReadLine();
+            while(pal!=null)
+            {
+                Console.WriteLine(pal);
+                Thread.Sleep(1000);
+                pal = sr.ReadLine();
+            }
+            sr.Close();
+           m.Dispose();
+        }
+    }
+}
